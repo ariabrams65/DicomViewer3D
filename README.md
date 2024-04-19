da86b59e5a40073df34af7ad8d6976efe8d09089
ariabrams65 <ariabrams65>
2024-04-19 12:41:28 -0700

    Python script is now executed from virtual environment. README also added

---
+++ README.md
@@ -0,0 +1,44 @@
+# DicomViewer3D
+
+Upload dicom images and view them in 3d.
+
+
+## Installation 
+
+Make sure you have both Node.js and Python installed
+
+**Clone the repository:**
+```
+git clone https://github.com/ariabrams65/DicomViewer3D
+```
+**Change into the directory and create a python virtual environment:**
+```
+python -m venv .venv
+```
+**Activate the virtual environment:**
+
+Windows:
+```
+.\.venv\Scripts\activate
+```
+
+Unix or MacOS:
+```
+source .venv/bin/activate
+```
+
+**Install the python dependencies:**
+```
+pip install -r requirements.txt
+```
+## How to run locally
+
+**Run the server:**
+
+```
+npm run start
+```
+Go to <http://localhost:3000> to access the webpage
+
+Note: after installation, you dont need to activate the python virtual environment
+every time. Just run the server.
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const child_process = require('child_process');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,12 +24,22 @@ app.use((req, res, next) => {
 });
 
 app.post('/dicom', upload.single('dicom'), (req, res) => {
-    const pathToScript = '../.local/lib/python3.10/site-packages/dicom2stl/Dicom2STL.py';
+    //Location of dicom2stl in virtual environment
+    const pathToScriptUnix = path.join(__dirname, '.venv', 'bin', 'dicom2stl');
+    const pathToScriptWin = path.join(__dirname, '.venv', 'Scripts', 'dicom2stl');
+    
+    //Location of python in virtual environment
+    const pythonEnvUnix = path.join(__dirname, '.venv', 'bin', 'python');
+    const pythonEnvWin = path.join(__dirname, '.venv', 'Scripts', 'python.exe');
+
+    const pythonEnv = process.platform === 'win32' ? pythonEnvWin : pythonEnvUnix;
+    const pathToScript = process.platform === 'win32' ? pathToScriptWin : pathToScriptUnix;
+
     const args = `-t bone --reduce 0 -o ./stl/output.stl ${req.file.path}`;
-    const result = child_process.execSync(`python3 ${pathToScript} ${args}`);
+    const result = child_process.execSync(`${pythonEnv} ${pathToScript} ${args}`);
     console.log(result.toString());
 
-    //downloading file for now but should do res.sendFile
+    //Just downloading the file for now but will eventually display 3d render in browser
     res.download('./stl/output.stl');
 });
 
