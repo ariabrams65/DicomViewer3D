1bcf4a62f09270d8b42458e5b8f2da2b135829eb
ariabrams65 <ariabrams65>
2024-04-21 00:47:27 -0700

    added vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     server: {
         proxy: {
-            '/api': {  // This is the prefix used for backend routes
-                target: 'http://localhost:3000',  // The backend server
-                changeOrigin: true,  // Necessary for virtual hosted sites
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
                 secure: false
             }
