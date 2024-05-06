c4ff8fce7b48616afea9d69f04768034d31ee84a
Elijah Hahm <ejhahm@uci.edu>
2024-05-06 01:58:28 -0700

    This is Scuffed

--- server/services/dicom.js
+++ server/services/dicom.js
@@ -19,10 +19,27 @@ async function generateSTL(filePath) {
         fs.mkdirSync(outputDir, {recursive: true});
     }
     const outputFile = path.join(outputDir, 'output.stl');
+    const outputFile2 = path.join(outputDir, 'output2.stl');
+    //const metaFile = path.join(outputDir, 'metadata.txt')
+    const outputFile3 = path.join(outputDir, 'output3.stl');
+    const outputFile4 = path.join(outputDir, 'output4.stl');
+    const outputFile5 = path.join(outputDir, 'output5.stl');
 
-    const args = `-t soft_tissue --reduce 0 -o ${outputFile} ${filePath}`;
+    const args = `-a -t soft_tissue --reduce 0 --smooth 25 -l -o ${outputFile} ${filePath}`;
+    const args2 = `-a -t skin --reduce 0 --smooth 25 -l -o ${outputFile2} ${filePath}`;
+    const args3 = `-a -t bone --reduce 0 --smooth 25 -l -o ${outputFile3} ${filePath}`;
+    const args4 = `-a -t fat --reduce 0 --smooth 25 -l -o ${outputFile4} ${filePath}`;
+    const args5 = `-a -t two --reduce 0 --smooth 25 -o ${outputFile5} ${filePath}`;
     const result = child_process.execSync(`${pythonEnv} ${pathToScript} ${args}`);
+    const result2 = child_process.execSync(`${pythonEnv} ${pathToScript} ${args2}`);
+    const result3 = child_process.execSync(`${pythonEnv} ${pathToScript} ${args3}`);
+    const result4 = child_process.execSync(`${pythonEnv} ${pathToScript} ${args4}`);
+    const result5 = child_process.execSync(`${pythonEnv} ${pathToScript} ${args5}`);
     console.log(result.toString());
+    console.log(result2.toString());
+    console.log(result3.toString());
+    console.log(result4.toString());
+    console.log(result5.toString());
 }
 
 module.exports = { generateSTL };
