d053a5b5ceadb93a0efc0ac55c59a9b605961076
Ari Abrams <ariabrams65@gmail.com>
2024-05-12 00:51:31 -0700

    gltf files are now generated using 3d slicer on the server

--- server/services/dicom.js
+++ server/services/dicom.js
@@ -4,25 +4,15 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const config = require('../../config');
 
-async function generateModels(dicomFolder) {
+async function generateModels(inputFolder) {
     //create the folder where the models will be stored
     const modelId = uuidv4();
-    const folder = createOutputFolder(modelId);
+    const outputFolder = createOutputFolder(modelId);
 
-    //create an array for the 4 argument strings for each tissue type
-    all_args = [];
-    for (const type of ['soft_tissue', 'skin', 'bone', 'fat']) {
-        const outputFile = path.join(folder, `${type}.stl`);
-        const args = `-a -t ${type} --reduce 0 --smooth 25 -l -o ${outputFile} ${dicomFolder}`;
-        all_args.push(args);
-    }
-
-    //run the script 4 times to create the models
-    const command = getScriptCommand();
-    for (const args of all_args) {
-        const result = child_process.execSync(`${command} ${args}`);
-        console.log(result.toString());
-    } 
+    //run the script to create the model
+    const command = getScriptCommand(inputFolder, outputFolder);
+    const result = child_process.execSync(command);
+    console.log(result.toString());
 
     return modelId;
 }
@@ -31,25 +21,16 @@ function createOutputFolder(modelId) {
     if (!fs.existsSync(config.modelFolder)) {
         fs.mkdirSync(config.modelFolder, {recursive: true});
     }
-    //Creating the folder where the 4 models will live
+    //Creating the folder where the model will live
     const folder = path.join(config.modelFolder, modelId);
     fs.mkdirSync(folder, {recursive: true});
     return folder;
 }
 
-function getScriptCommand() {
-    //Location of dicom2stl in virtual environment
-    const pathToScriptUnix = path.join(__dirname, '..', '.venv', 'bin', 'dicom2stl');
-    const pathToScriptWin = path.join(__dirname, '..', '.venv', 'Scripts', 'dicom2stl.exe');
-    
-    //Location of python in virtual environment
-    const pythonEnvUnix = path.join(__dirname, '..', '.venv', 'bin', 'python');
-    const pythonEnvWin = path.join(__dirname, '..', '.venv', 'Scripts', 'python.exe');
-
-    const pythonEnv = process.platform === 'win32' ? pythonEnvWin : pythonEnvUnix;
-    const pathToScript = process.platform === 'win32' ? pathToScriptWin : pathToScriptUnix;
-
-    return `${pythonEnv} ${pathToScript}`;
+function getScriptCommand(inputFolder, outputFolder) {
+    const scriptPath =  `${path.join(__dirname, '../slicer/script.py')}`;
+    const scriptArgs = `${path.join(config.dicomFolder, inputFolder)} ${path.join(config.modelFolder, outputFolder)}`;
+    return `"${process.env.SLICER_EXECUTABLE}" --python-script ${scriptPath} ${scriptArgs}`;
 }
 
 function isValid(dicomFolder) {
---
+++ server/slicer/script.py
@@ -0,0 +1,19 @@
+from DICOMLib import DICOMUtils
+import sys
+import os
+
+if __name__ == '__main__':
+    dicomDataDir = sys.argv[1] # input folder with DICOM files
+    loadedNodeIDs = []  # this list will contain the list of all loaded node IDs
+
+    with DICOMUtils.TemporaryDICOMDatabase() as db:
+        DICOMUtils.importDicom(dicomDataDir, db)
+        patientUIDs = db.patients()
+        for patientUID in patientUIDs:
+            loadedNodeIDs.extend(DICOMUtils.loadPatientByUID(patientUID))
+
+
+    exporter = vtk.vtkGLTFExporter()
+    exporter.SetRenderWindow(slicer.app.layoutManager().threeDWidget(0).threeDView().renderWindow())
+    exporter.SetFileName(os.path.join(sys.argv[2], 'model.gltf'))
+    exporter.Write()
