7d178d6c3428b2c6fe65002351ee260850ffce3f
David Chan <davidochan@gmail.com>
2024-05-13 00:47:56 -0700

    added skeleton for filter zip file functions

--- server/services/dicom.js
+++ server/services/dicom.js
@@ -6,54 +6,54 @@ const AdmZip = require("adm-zip");
 const config = require("../../config");
 
 async function generateModels(inputFolder) {
-  //create the folder where the models will be stored
-  const modelId = uuidv4();
-  const outputFolder = createOutputFolder(modelId);
+	//create the folder where the models will be stored
+	const modelId = uuidv4();
+	const outputFolder = createOutputFolder(modelId);
 
-  //run the script to create the model
-  const command = getScriptCommand(inputFolder, outputFolder);
-  const result = child_process.execSync(command);
-  console.log(result.toString());
+	//run the script to create the model
+	const command = getScriptCommand(inputFolder, outputFolder);
+	const result = child_process.execSync(command);
+	console.log(result.toString());
 
-  return modelId;
+	return modelId;
 }
 
 function createOutputFolder(modelId) {
-  //Creating the folder where the model will live
-  const folder = path.join(config.modelFolder, modelId);
-  fs.mkdirSync(folder, { recursive: true });
-  return folder;
+	//Creating the folder where the model will live
+	const folder = path.join(config.modelFolder, modelId);
+	fs.mkdirSync(folder, { recursive: true });
+	return folder;
 }
 
 function getScriptCommand(inputFolder, outputFolder) {
-  const scriptPath = `${path.join(__dirname, "../slicer/script.py")}`;
-  const scriptArgs = `${inputFolder} ${outputFolder}`;
-  return `"${process.env.SLICER_EXECUTABLE}" --python-script ${scriptPath} ${scriptArgs}`;
+	const scriptPath = `${path.join(__dirname, "../slicer/script.py")}`;
+	const scriptArgs = `${inputFolder} ${outputFolder}`;
+	return `"${process.env.SLICER_EXECUTABLE}" --python-script ${scriptPath} ${scriptArgs}`;
 }
 
 function isValid(dicomFolder) {
-  //returns false if there aren't enough .dcm files in folder
-  return true;
+	//returns false if there aren't enough .dcm files in folder
+	return true;
 }
 
 function filterDicom(dicomFolder) {
-  // filter folder to only contain dcm files
-  // result should remove non dcm files
+	// filter folder to only contain dcm files
+	// result should remove non dcm files
 }
 
 function isZip(file) {
-  //returns true if the given file is a zip archive
-  return true;
+	//returns true if the given file is a zip archive
+	return true;
 }
 
 function unzip(zipFile) {
-  //unzips file into config.dicomFolder and returns the subfolder path
-  const zip = new AdmZip(zipFile);
-  const targetFolder = path.join(config.dicomFolder, uuidv4());
-  fs.mkdirSync(targetFolder, { recursive: true });
+	//unzips file into config.dicomFolder and returns the subfolder path
+	const zip = new AdmZip(zipFile);
+	const targetFolder = path.join(config.dicomFolder, uuidv4());
+	fs.mkdirSync(targetFolder, { recursive: true });
 
-  zip.extractAllTo(targetFolder, true);
-  return targetFolder;
+	zip.extractAllTo(targetFolder, true);
+	return targetFolder;
 }
 
 module.exports = { generateModels, isValid, unzip, isZip, filterDicom };
