14bfd270b60bfb198efd7de20814924d01c251fe
David Chan <davidochan@gmail.com>
2024-06-02 14:56:36 -0700

    adding server.js to react-client to manually bind port number with render

---
+++ react-client/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname with ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
